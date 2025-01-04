b3c9fb7d4081bcaaf43086b6a1cd1b9232768ffe
ddubi-dev <1kimc@naver.com>
2025-01-04 09:38:03 +0900

    feat: 팝업올리기 로그인된 사용자에게만 권한

--- frontend/src/components/CreatePopup.jsx
+++ frontend/src/components/CreatePopup.jsx
@@ -1,9 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import useAuth from "../context/useAuth";
 
 const CreatePopup = () => {
+  const { auth } = useAuth(); // 로그인 상태 가져오기
   const navigate = useNavigate();
 
   const handleClick = () => {
+    if (!auth.isLoggedIn) {
+      alert("로그인된 사용자만 팝업을 올릴 수 있습니다.\n로그인 페이지로 이동합니다.");
+      navigate("login");
+      return;
+    }
+
     navigate("registerPopup");
   };
 
