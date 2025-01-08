1b578dcb965535a4e54fc36ab9cedfa57d115a30
cheon1217 <friendly1217@naver.com>
2025-01-08 17:11:28 +0900

    error 해결

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/users");
 const emailVerificationRoutes = require("./routes/emailVerification");
