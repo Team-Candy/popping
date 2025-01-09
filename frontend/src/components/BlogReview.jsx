c96c2d6bf8ce33d92992f59f6a99ba5437c292dd
ddubi-dev <1kimc@naver.com>
2025-01-09 17:53:35 +0900

    통합

--- frontend/src/components/BlogReview.jsx
+++ frontend/src/components/BlogReview.jsx
@@ -12,27 +12,26 @@ const BlogReview = ({ name }) => {
         throw new Error("Failed to fetch blogs data");
       }
 
-      // const data = await response.json();
+      console.log("response: ", response);
+      const data = await response.json();
 
-      // 임시 데이터
-      const data = {
-        result: [
-          {
-            title: "블로그 제목 1",
-            link: "https://blog.example.com/1",
-            description: "이 블로그는 JavaScript에 대한 내용을 다룹니다.",
-            postdate: "2024-12-01",
-          },
-          {
-            title: "블로그 제목 2",
-            link: "https://blog.example.com/2",
-            description: "React를 사용한 프로젝트 경험 공유.",
-            postdate: "2024-11-30",
-          },
-        ],
-      };
-
-      console.log(data);
+      // // 임시 데이터
+      // const data = {
+      //   result: [
+      //     {
+      //       title: "블로그 제목 1",
+      //       link: "https://blog.example.com/1",
+      //       description: "이 블로그는 JavaScript에 대한 내용을 다룹니다.",
+      //       postdate: "2024-12-01",
+      //     },
+      //     {
+      //       title: "블로그 제목 2",
+      //       link: "https://blog.example.com/2",
+      //       description: "React를 사용한 프로젝트 경험 공유.",
+      //       postdate: "2024-11-30",
+      //     },
+      //   ],
+      // };
 
       if (data.result && Array.isArray(data.result)) {
         // {”result” : [{”title”: “(블로그 글 제목)”, “link”: “(블로그 포스트의 URL)”, “description”:”(블로그 포스트 내용 요약 정보)”, “postdate”:”(블로그 포스트 작성된 날짜)” }]}
@@ -41,7 +40,7 @@ const BlogReview = ({ name }) => {
         setBlogs([]); // result가 없거나, []이 아닐 경우
       }
     } catch (err) {
-      console.err("Error fetching blogs:", err.message);
+      console.error("Error fetching blogs:", err.message);
       setBlogs([]);
     }
   };
